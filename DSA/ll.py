ad48f1e33fcc6d021f4fe33366b83a56ecef0e42
sreeram0343 <sreerammurali2005@gmail.com>
2026-07-27 19:43:14 +0530

    LinkedList

---
+++ DSA/ll.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def __repr__(self):
+        pass
+
+    def __contains__(self, item):
+        pass
+
+    def __len__(self):
+        pass
+
+
+                
